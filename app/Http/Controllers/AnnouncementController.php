21fb9c078ab0bacaf37f60c8c0ea8a4496e2e192
Stefano Catalano <ste_fano91@hotmail.it>
2020-11-01 12:24:22 +0100

    inserimento selezione categoria e memorizza categoria nel database

---
+++ app/Http/Controllers/AnnouncementController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Category;
+use App\Models\Announcement;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
+use App\Http\Requests\AnnouncementRequest;
+
+class AnnouncementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function create()
+    {
+        $categories = Category::all();
+        View::share('categories', $categories);
+
+        return view('announcement.new');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(AnnouncementRequest $request)
+    {
+        $title = $request->input('title');
+        $body = $request->input('body');
+        $category_id = $request->input('category_id');
+
+
+        $post = Announcement::create([
+           'title' => $title,
+           'body' => $body,
+           'category_id' => $category_id,
+           'user_id' => Auth::user()->id
+           ]);
+
+        return redirect(route('home'))->with('message', "il tuo post è stato aggiunto all' elenco");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(announcement $announcement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(announcement $announcement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, announcement $announcement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(announcement $announcement)
+    {
+        //
+    }
+}
---
+++ app/Models/Announcement.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Announcement extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+         'title', 'body',     //protezione per il Models per evitare dati indesiderati
+    ];                   
+
+     public function user()
+      {
+          return $this->belongsTo(User::class);
+      }
+
+      public function category()
+      {
+          return $this->belongsTo(Category::class);
+      }
+}
