8cb52e7cdc7355286c8db9117b428b733c957c55
Stefano Catalano <ste_fano91@hotmail.it>
2020-11-03 09:36:26 +0100

    validation rules

--- app/Http/Requests/AnnouncementRequest.php
+++ app/Http/Requests/AnnouncementRequest.php
@@ -26,8 +26,8 @@ class AnnouncementRequest extends FormRequest
         return [
             'title' => "required|max:120",
             'body' => "required|max: 1000",
-            'user_id' => "required",
-            'category_id' => "required",
+            // 'user_id' => "required",
+            // 'category_id' => "required",
         ];
     }
 
