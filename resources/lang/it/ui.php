f4aa6bd09d7107fbe60d6044d1a1d51885d91626
Gianni Mondello <gianni.mondello@live.it>
2020-11-10 10:06:28 +0100

    Lingue

--- resources/lang/it/ui.php
+++ resources/lang/it/ui.php
@@ -2,9 +2,16 @@
 
 return [
 
+ //Vista welcome
+
  'welcome' => 'Benvenuti in presto.it',
  'mostPopular' => 'In evidenza',
  'categories' => 'Categorie',
  'explore' => 'Esplora le nostre sezioni',
- 'add' => 'inserisci annuncio'
+ 'add' => 'inserisci annuncio',
+  'signIn' => 'Accedi',
+   'signUp' => 'Registrati'
+
+   //Fine welcome
+
 ];
