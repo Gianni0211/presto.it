52d68feca1914812ca47f0a10097e77a408fb2d3
Micaela Cataldo <cataldomicaela@gmail.com>
2020-11-12 19:57:01 +0100

    modifiche varie

--- resources/views/category/announcements.blade.php
+++ resources/views/category/announcements.blade.php
@@ -5,28 +5,25 @@
 title="{{ $category->name }}"
 
 />
-    {{-- <div class="container my-5 py-5">
-        <div class="row justify-content-center">
-            <div class="col-12 col-md-8">
-                <h1>Annunci per categoria: {{ $category->name }}</h1>
-            </div>
-        </div>
-    </div> --}}
+
     
     
-    <div class="container-fluid py-3 overflowSliderBG">{{--  fix carta categorie --}}
-        <div class="container">
+    
+        <div class="container py-3 mt-5">
             
-            <section id="overflowSlider" class="w-100">
-                <div class="row py-5">
-                    <div class="col">
-                        <h2 class="txtTitle font-italic h1 text-center">In evidenza</h2>
-                    </div>
-                </div>
-                
             <div class="row flex-nowrap overflow-auto">
-                    
+                
                 <x-sidebar-category />
+                    <section id="overflowSlider" class="w-100">
+                        <div class="search-box-layout1 mb-4 rounded">
+                            <form class="form-inline" method="GET" action="{{ route('search') }}">
+                              <input class="form-control input-search" name="q" type="search" placeholder="{{ __('ui.searchFor') }}" aria-label="Search">
+                              <button class="bg-orange my-2 my-sm-0 btn-search text-white border-0 rounded" type="submit">
+                                <i class="fas fa-search txt-shadow"></i> <span class="txt-shadow">{{ __('ui.search') }}</span>
+                              </button>
+                            </form>
+                    
+                          </div>
           @foreach ($announcements as $announcement)
           <x-card 
            :announcement="$announcement"
@@ -36,7 +33,7 @@ title="{{ $category->name }}"
         </div>
       </section>
       </div>
-    </div>
+    
    
 
    
--- resources/views/components/sidebar-category.blade.php
+++ resources/views/components/sidebar-category.blade.php
@@ -1,19 +1,16 @@
 <div class="col-12 col-md-12 col-lg-3">
-    <div class="row">
-
-        
-            
+   
         <div class="category-box-layout2">
             <div class="item-heading-area">
                 
-                <h3 class="item-title">Categorie</h3>
+                <h3 class="item-title text-white">Categorie</h3>
             </div>
             
             <ul class="item-sub-category">
 
                 @foreach ($categories as $category)
-                <li>
-                    <a href="{{ route('category.announcements', [
+                <li class="mb-3">
+                    <a class="text-decoration-none txtTitle hover-a" href="{{ route('category.announcements', [
                   $category->name,
                   $category->id
                    ]) }}">{{ $category->name }}   ( {{ $category->announcementCount() }})</a>
@@ -23,7 +20,9 @@
                
             </ul>
         </div>
-    
-    </div>
+                        <div class="col-12 mt-4">
+                            <a class="nav-link bg-yellow text-white rounded btn-hover text-center lead" href="{{ route('announcement.new') }}">{{ __('ui.add') }}  {{-- <i class="fas fa-plus"></i> --}}</a>
+                        </div>
+   
     
 </div>
